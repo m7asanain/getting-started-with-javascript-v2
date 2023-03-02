e3fd12b9b3ff494797a202056a923c243dc84f9f
Mustafa Alkamali <m7asanain@gmail.com>
2023-03-02 18:12:27 +0300

    fixed

--- ex.js
+++ ex.js
@@ -1,6 +1,21 @@
 // TODO: define addFavoriteBook(..) function
+function addFavoriteBook(bookName) {
+  if (!bookName.includes("Great")) {
+    favoriteBooks.push(bookName);
+  }
+}
 
 // TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+  console.log("Favorite Books: ..");
+  for (let i = 0; i < favoriteBooks.length; i++) {
+    console.log(`${i + 1}- ${favoriteBooks[i]}`);
+  }
+
+  // for (let bookName of favoriteBooks) {
+	// 	console.log(bookName);
+	// }
+}
 
 var favoriteBooks = [];
 
@@ -11,3 +26,5 @@ addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 // TODO: print out favorite books
+
+printFavoriteBooks();
